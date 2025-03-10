1fd68a04e54f094b5d32b4d5816764eb9bf49f4b
wellington-workstation <rodrigueswellington3@gmail.com>
2025-03-10 17:13:27 -0300

    Teste no endpoint findAll, buscando produtos por nome

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -12,10 +12,13 @@ import org.junit.jupiter.api.Test;
 public class ProductControllerRA {
 
 	private Long existingProductId, nonExistingProductId;
+	private String productName;
 	
 	@BeforeEach
 	public void setUp() {
 		baseURI = "http://localhost:8080";
+		
+		productName = "Macbook";
 	}
 	
 	@Test
@@ -32,5 +35,35 @@ public class ProductControllerRA {
 			.body("price", is(2190.0F))
 			.body("categories.id", hasItems(2, 3))
 			.body("categories.name", hasItems("Eletrônicos", "Computadores"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWhenProductNameIsEmpty() {
+		given()
+			.get("/products?page=0")
+		.then()
+			.statusCode(200)
+			.body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWhenProductNameIsNotEmpty() {
+		given()
+			.get("/products?name={productName}", productName)
+		.then()
+			.statusCode(200)
+			.body("content.id[0]", is(3))
+			.body("content.name[0]", equalTo("Macbook Pro"))
+			.body("content.price[0]", is(1250.0F))
+			.body("content.imgUrl[0]", equalTo( "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000() {
+		given()
+			.get("/products?size=25")
+		.then()
+			.statusCode(200)
+			.body("content.findAll { it.price > 2000}.name", hasItems("Smart TV", "PC Gamer Weed"));
 	}
 }
