118dc56cb3aff715ffa0c47e015f961457369831
wellington-workstation <rodrigueswellington3@gmail.com>
2025-03-10 16:15:52 -0300

    Teste consultar pedido por id

---
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProductControllerRA {
+
+	private Long existingProductId, nonExistingProductId;
+	
+	@BeforeEach
+	public void setUp() {
+		baseURI = "http://localhost:8080";
+	}
+	
+	@Test
+	public void findByIdShouldReturnProductWhenIdExists() {
+		existingProductId = 2L;
+		
+		given()
+			.get("/products/{id}", existingProductId)
+		.then()
+			.statusCode(200)
+			.body("id", is(2))
+			.body("name", equalTo("Smart TV"))
+			.body("imgUrl", equalTo( "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
+			.body("price", is(2190.0F))
+			.body("categories.id", hasItems(2, 3))
+			.body("categories.name", hasItems("Eletrônicos", "Computadores"));
+	}
+}
