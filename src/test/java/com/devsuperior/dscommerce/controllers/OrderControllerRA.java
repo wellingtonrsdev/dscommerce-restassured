58f26047a2433f98fd21918353365939f8f1227c
wellington-workstation <rodrigueswellington3@gmail.com>
2025-03-20 08:48:41 -0300

    teste consulta pedidos  por id, retorna o pedido quando admin logado

---
+++ src/test/java/com/devsuperior/dscommerce/controllers/OrderControllerRA.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dscommerce.controllers;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.devsuperior.dscommerce.tests.TokenUtil;
+
+import io.restassured.http.ContentType;
+
+public class OrderControllerRA {
+
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private String clientToken, adminToken, invalidToken;
+	private Long existingOrderId, nonExistingOrderId, dependentOrderId;
+
+	@BeforeEach
+	public void setUp() {
+		baseURI = "http://localhost:8080";
+		
+		existingOrderId = 1L;
+		nonExistingOrderId = 100L;
+
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
+		adminUsername = "alex@gmail.com";
+		adminPassword = "123456";
+
+		clientToken = TokenUtil.obtainAcessToken(clientUsername, clientPassword);
+		adminToken = TokenUtil.obtainAcessToken(adminUsername, adminPassword);
+		invalidToken = adminToken + "xpto"; // Invalid token
+	}
+	
+	@Test
+	public void findByIdShouldReturnOrderWhenIdExistsAndAdminLogged() {
+		
+		given()
+			.header("Content-Type", "application/json")
+			.header("Authorization", "Bearer " + adminToken)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/orders/{id}", existingOrderId)
+		.then()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("moment", equalTo("2022-07-25T13:00:00Z"))
+			.body("status", equalTo("PAID"))
+			.body("client.name", equalTo( "Maria Brown"))
+			.body("payment.moment", equalTo("2022-07-25T15:00:00Z"))
+			.body("items.name", hasItems("The Lord of the Rings", "Macbook Pro"))
+			.body("total", is(1431.0F));
+	}
+}
